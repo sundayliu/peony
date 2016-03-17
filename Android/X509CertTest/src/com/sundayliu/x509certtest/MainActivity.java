2c4a0e785fabb95cb119af012a6d596bb1d8fc93
sundayliu <martinkro@aliyun.com>
2016-03-17 17:43:31 +0800

    test

--- Android/X509CertTest/src/com/sundayliu/x509certtest/MainActivity.java
+++ Android/X509CertTest/src/com/sundayliu/x509certtest/MainActivity.java
@@ -3,15 +3,27 @@ package com.sundayliu.x509certtest;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyFactory;
 import java.security.MessageDigest;
+import java.security.Principal;
+import java.security.PublicKey;
 import java.security.Signature;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
+//import android.content.pm.Signature;
 import android.util.Log;
 import android.view.Menu;
+
+
 
 public class MainActivity extends Activity {
     private static final String DEBUG_TAG = "X509CertTest";
@@ -21,7 +33,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         verifyCert();
+        //dumpInfo();
         //Log.e(DEBUG_TAG, "Verify result:" + result);
+        //DerInputStream s;
     }
 
     @Override
@@ -31,23 +45,90 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void parseSignature(byte[] signature){
+        try{
+            
+            Log.e(DEBUG_TAG, "size:" + signature.length);
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            
+            X509Certificate cert = (X509Certificate)cf.generateCertificate(new ByteArrayInputStream(signature));
+            Principal  p = cert.getSubjectDN();
+            Log.e(DEBUG_TAG, p.getName());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void dumpInfo(){
+        String packageName = getPackageName();
+        Log.e(DEBUG_TAG, packageName);
+        
+        String packagePath = getPackageResourcePath();
+        Log.e(DEBUG_TAG, packagePath);
+        try{
+            PackageManager pkgMgr = getPackageManager();
+            if (pkgMgr == null){
+                Log.e(DEBUG_TAG, "pkgMgr is null");
+            }
+            PackageInfo pkgInfo = pkgMgr.getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+            
+            if (pkgInfo == null){
+                Log.e(DEBUG_TAG, "pkgInfo is null");
+            }
+            ProviderInfo[] providers = pkgInfo.providers;
+            android.content.pm.Signature[] signatures = pkgInfo.signatures;
+            if (providers == null){
+                Log.e(DEBUG_TAG, "providers is null");
+            }
+            else{
+                for (int i = 0; i < providers.length; i++){
+                    ProviderInfo provider = providers[i];
+                    Log.e(DEBUG_TAG, "Provider:" + provider.name);
+                }
+            }
+            
+            if (signatures == null){
+                Log.e(DEBUG_TAG, "signature is null");
+            }
+            else{
+                for (int i = 0; i < signatures.length; i++){
+                    android.content.pm.Signature signature = signatures[i];
+                    parseSignature(signature.toByteArray());
+                    
+                }
+            }
+            
+            
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
     public boolean verifyCert(){
         boolean ret = verifyCert("test.SF", "test_signed.bin", "test_cert.der", "SHA1withRSA");
+        verifyCert("test.SF", "test_signed.bin", "test_cert.der", "SHA1", "RSA");
         Log.e(DEBUG_TAG, "Verify TEST result:" + ret);
         
-        ret = verifyCert("RSA.SF", "RSA.sign", "RSA.der", "SHA1withRSA");
-        Log.e(DEBUG_TAG, "Verify RSA result:" + ret);
+        //ret = verifyCert("RSA.SF", "RSA.sign", "RSA.der", "SHA1withRSA");
+        verifyCert("RSA.SF", "RSA.sign", "RSA.der", "SHA1", "RSA");
+        //Log.e(DEBUG_TAG, "Verify RSA result:" + ret);
         
-        ret = verifyCert("DSA.SF", "DSA.sign", "DSA.der", "SHA1withDSA");
-        Log.e(DEBUG_TAG, "Verify DSA result:" + ret);
+        //ret = verifyCert("DSA.SF", "DSA.sign", "DSA.der", "SHA1withDSA");
+        //verifyCert("DSA.SF", "DSA.sign", "DSA.der", "SHA1", "DSA");
+        //Log.e(DEBUG_TAG, "Verify DSA result:" + ret);
         
-        ret = verifyCert("EC.SF", "EC.sign", "EC.der", "SHA256withECDSA");
-        Log.e(DEBUG_TAG, "Verify ECDSA result:" + ret);
+        //ret = verifyCert("EC.SF", "EC.sign", "EC.der", "SHA256withECDSA");
+        //verifyCert("EC.SF", "EC.sign", "EC.der", "SHA256", "ECDSA");
+        //Log.e(DEBUG_TAG, "Verify ECDSA result:" + ret);
         return false;
     }
     
     public boolean verifyCert(String signDataName, String signedDataName, String certName, String algorithm){
-        
+        //return true;
         boolean result = false;
         try{
         InputStream certStream = getAssets().open(certName);
@@ -72,6 +153,49 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
         return result;
+    }
+    
+    public boolean verifyCert(String signFileName, String signedFileName, String certFileName, String algDigest, String algEncrypt){
+        try{
+            InputStream isSign = getAssets().open(signFileName);
+            byte[] signData = new byte[isSign.available()];
+            isSign.read(signData);
+            isSign.close();
+            
+            MessageDigest digestInstance = MessageDigest.getInstance(algDigest);
+            digestInstance.update(signData);
+            byte[] hash = digestInstance.digest();
+            Log.e(DEBUG_TAG, "HASH:" + hexEncode(hash));
+            
+            InputStream isSigned = getAssets().open(signedFileName);
+            byte[] signedData = new byte[isSigned.available()];
+            isSigned.read(signedData);
+            isSigned.close();
+            
+            Log.e(DEBUG_TAG, "signature data size:" + signedData.length);
+            
+            InputStream isCert = getAssets().open(certFileName);
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate)cf.generateCertificate(isCert);
+            isCert.close();
+            
+            PublicKey publicKey = cert.getPublicKey();
+            
+            byte[] decryptData = decryptByPublicKey(signedData, publicKey, algEncrypt);
+            Log.e(DEBUG_TAG, "DECRYPT size:" + decryptData.length);
+            Log.e(DEBUG_TAG, "DECRYPT:" + hexEncode(decryptData));
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    
+    public static byte[] decryptByPublicKey(byte[] data, PublicKey key, String algName) throws Exception{
+        Cipher cipher = Cipher.getInstance(algName);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(data);
     }
     
     public static boolean verifySignedData(byte[] signData, byte[] signedData, byte[] cert, String algorithm){
--- java/X509Test/src/com/sundayliu/x509/Main.java
+++ java/X509Test/src/com/sundayliu/x509/Main.java
@@ -4,19 +4,271 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.ProviderException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
 
+//import sun.security.rsa.*;
 
+class RSACore{
+	public static int getByteLength(BigInteger b){
+		int n = b.bitLength();
+		return (n + 7) >> 3;
+	}
+	
+	public static int getByteLength(RSAKey key){
+		return getByteLength(key.getModulus());
+	}
+	
+    /**
+     * Perform an RSA public key operation.
+     */
+    public static byte[] rsa(byte[] msg, RSAPublicKey key)
+            throws BadPaddingException {
+        return crypt(msg, key.getModulus(), key.getPublicExponent());
+    }
+    
+    /**
+     * RSA public key ops and non-CRT private key ops. Simple modPow().
+     */
+    private static byte[] crypt(byte[] msg, BigInteger n, BigInteger exp)
+            throws BadPaddingException {
+        BigInteger m = parseMsg(msg, n);
+        BigInteger c = m.modPow(exp, n);
+        return toByteArray(c, getByteLength(n));
+    }
+    
+    /**
+     * Parse the msg into a BigInteger and check against the modulus n.
+     */
+    private static BigInteger parseMsg(byte[] msg, BigInteger n)
+            throws BadPaddingException {
+        BigInteger m = new BigInteger(1, msg);
+        if (m.compareTo(n) >= 0) {
+            throw new BadPaddingException("Message is larger than modulus");
+        }
+        return m;
+    }
+
+    /**
+     * Return the encoding of this BigInteger that is exactly len bytes long.
+     * Prefix/strip off leading 0x00 bytes if necessary.
+     * Precondition: bi must fit into len bytes
+     */
+    private static byte[] toByteArray(BigInteger bi, int len) {
+        byte[] b = bi.toByteArray();
+        int n = b.length;
+        if (n == len) {
+            return b;
+        }
+        // BigInteger prefixed a 0x00 byte for 2's complement form, remove it
+        if ((n == len + 1) && (b[0] == 0)) {
+            byte[] t = new byte[len];
+            System.arraycopy(b, 1, t, 0, len);
+            return t;
+        }
+        // must be smaller
+        assert (n < len);
+        byte[] t = new byte[len];
+        System.arraycopy(b, 0, t, (len - n), n);
+        return t;
+    }
+}
+class RSASignature{
+    private static final int baseLength = 8;
+
+    // object identifier for the message digest algorithm used
+    //private final ObjectIdentifier digestOID;
+
+    // length of the encoded signature blob
+    private final int encodedLength;
+
+    // message digest implementation we use
+    private final MessageDigest md;
+    // flag indicating whether the digest is reset
+    private boolean digestReset;
+
+    // private key, if initialized for signing
+    private RSAPrivateKey privateKey;
+    // public key, if initialized for verifying
+    private RSAPublicKey publicKey;
+
+    // padding to use, set when the initSign/initVerify is called
+    //private RSAPadding padding;
+	
+    /**
+     * Construct a new RSASignature. Used by subclasses.
+     */
+    RSASignature(String algorithm,  int oidLength) {
+        //this.digestOID = digestOID;
+        try {
+            md = MessageDigest.getInstance(algorithm);
+            //AlgorithmId x
+        } catch (NoSuchAlgorithmException e) {
+            throw new ProviderException(e);
+        }
+        digestReset = true;
+        encodedLength = baseLength + oidLength + md.getDigestLength();
+    }
+
+    // initialize for verification. See JCA doc
+    public void engineInitVerify(PublicKey publicKey)
+            throws InvalidKeyException {
+        RSAPublicKey rsaKey = (RSAPublicKey)(publicKey);
+        this.privateKey = null;
+        this.publicKey = rsaKey;
+        initCommon(rsaKey, null);
+    }
+    
+    private void initCommon(RSAKey rsaKey, SecureRandom random)
+            throws InvalidKeyException {
+        //resetDigest();
+//        int keySize = RSACore.getByteLength(rsaKey);
+//        try {
+//            padding = RSAPadding.getInstance
+//                (RSAPadding.PAD_BLOCKTYPE_1, keySize, random);
+//        } catch (InvalidAlgorithmParameterException iape) {
+//            throw new InvalidKeyException(iape.getMessage());
+//        }
+//        int maxDataSize = padding.getMaxDataSize();
+//        if (encodedLength > maxDataSize) {
+//            throw new InvalidKeyException
+//                ("Key is too short for this signature algorithm");
+//        }
+    }
+	
+	public boolean engineVerify(byte[] sigBytes) throws SignatureException{
+		if (sigBytes.length != RSACore.getByteLength(publicKey)){
+			return false;
+		}
+		
+		try{
+			byte[] decrypted = RSACore.rsa(sigBytes, publicKey);
+			
+			System.out.println("size:" + decrypted.length);
+		}
+		catch(Exception e){
+			
+		}
+		return false;
+	}
+}
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,World!");
-		parsePKCS7("test.RSA", "test.SF");
+		//parsePKCS7("test.RSA", "test.SF");
+		
+		//RSASignature s;
+		//DerInputStream s;
+		
+		//verifyCert("test.SF","test_signed.bin","test_cert.der","SHA1","RSA");
+		verifyCert("CERT.SF","signed-sha1.bin","cert.cer","SHA1","RSA");
 	}
+	
+    public static String hexEncode(byte[] a){
+        StringBuilder sb = new StringBuilder();
+        for (byte b:a)
+            sb.append(String.format("%02x", b & 0xff));
+       return sb.toString();
+    }
+    
+    public static byte toHex(byte m){
+    	if (m >= '0' && m < '9'){
+			m = (byte)(m - '0');
+		}
+		else if (m >= 'A' && m <= 'Z'){
+			m = (byte)(m - 'A' + 10);
+		}
+		else if (m >= 'a' && m <= 'z'){
+			m = (byte)(m - 'a' + 10);
+		}
+    	return m;
+    }
+    
+    public static byte[] hexDecode(byte[] a){
+    	byte[] result = new byte[a.length/2];
+    	for (int i = 0; i < a.length; i+=2){
+    		byte m = a[i];
+    		byte n = a[i+1];
+    		m = toHex(m);
+    		n = toHex(n);
+    		
+    		result[i/2] = (byte)(m * 16 + n);
+    	}
+    	return result;
+    }
+	
+	public static boolean verifyCert(String signFileName, String signedFileName, String certFileName, String algDigest, String algEncrypt){
+        try{
+            FileInputStream isSign = new FileInputStream(new File(signFileName));
+            byte[] signData = new byte[isSign.available()];
+            isSign.read(signData);
+            isSign.close();
+            
+            MessageDigest digestInstance = MessageDigest.getInstance(algDigest);
+            digestInstance.update(signData);
+            byte[] hash = digestInstance.digest();
+            System.out.println( "HASH size:" + hash.length);
+            System.out.println( "HASH:" + hexEncode(hash));
+            
+            FileInputStream isSigned = new FileInputStream(new File(signedFileName));
+            byte[] signedData = new byte[isSigned.available()];
+            isSigned.read(signedData);
+            isSigned.close();
+            
+           System.out.println("signature data size:" + signedData.length);
+            
+            InputStream isCert = new FileInputStream(new File(certFileName));
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate)cf.generateCertificate(isCert);
+            isCert.close();
+            
+            PublicKey publicKey = cert.getPublicKey();
+            
+            byte[] decryptData = decryptByPublicKey(signData,signedData, publicKey, algEncrypt);
+            System.out.println("DECRYPT size:" + decryptData.length);
+            System.out.println("DECRYPT:" + hexEncode(decryptData));
+            
+//            byte[] signedData2 = hexDecode(signedData);
+//            decryptData = decryptByPublicKey(signedData2, publicKey, algEncrypt);
+//            System.out.println("DECRYPT size:" + decryptData.length);
+//            System.out.println("DECRYPT:" + hexEncode(decryptData));
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    
+    public static byte[] decryptByPublicKey(byte[] signData, byte[] data, PublicKey key, String algName) throws Exception{
+        Cipher cipher = Cipher.getInstance(algName);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        //byte[] hash = cipher.update(signData, 0, signData.length);
+        //System.out.println("DECRYPT size:" + hash.length);
+        //System.out.println("DECRYPT:" + hexEncode(hash));
+        byte[] out = cipher.doFinal(data);
+        
+        RSASignature rsa = new RSASignature("SHA1", 7);
+        rsa.engineInitVerify(key);
+        rsa.engineVerify(data);
+        return out;
+    }
 	
 	public static void parsePKCS7(String pkcs7Path, String signPath){
 		try{
